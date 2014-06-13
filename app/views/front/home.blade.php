c6d7f82016fd4b5146c2978b46fa7d9cd814dc81
Doraemonext <doraemonext@gmail.com>
2014-06-14 04:05:46 +0800

    修复显示数量的BUG

--- app/views/front/home.blade.php
+++ app/views/front/home.blade.php
@@ -64,7 +64,7 @@
                     <div class="investor_content">
                         <div class="investor_mscore">
                             <div class="investor_tscore">{{ round($vc->rating, 1) }}</div>
-                            <div class="investor_np">{{ $vc->ratings()->count() }}人打分</div>
+                            <div class="investor_np">{{ $vc->ratings()->where('vc_rating_category_id', '=', 1)->count() }}人打分</div>
                         </div>
                         <ul class="investor_detail">
                             @foreach ($rating_category as $category)
